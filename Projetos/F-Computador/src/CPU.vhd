a128c284f4e1d19f97d7c032ba9418e31feabdcc
pcliquet <pedroca8@al.insper.edu.br>
2022-05-11 16:26:04 -0300

    CPU nova

--- Projetos/F-Computador/src/CPU.vhd
+++ Projetos/F-Computador/src/CPU.vhd
@@ -101,7 +101,6 @@ architecture arch of CPU is
 begin
   OURO: ControlUnit port map (instruction,c_zr,c_ng,c_muxALUI_A,c_muxAM,c_zx,c_nx,c_zy,c_ny,c_f,c_no,c_loadA,c_loadD,writeM,c_loadPC);
 
-
   MuxALUI: Mux16 port map (s_ALUout,instruction(15 downto 0),c_muxALUI_A,s_muxALUI_Aout);
   
   REGA: Register16 port map (clock,s_muxALUI_Aout,c_loadA,s_regAout);
@@ -110,7 +109,7 @@ begin
 
   REGD: Register16 port map (clock,s_ALUout,c_loadD,s_regDout);  
 
-  ULA: ALU port map (s_regDout,s_regAout,c_zx,c_nx,c_zy,c_ny,c_f,c_no,c_zr,c_ng,s_ALUout);
+  ULA: ALU port map (s_regDout,s_muxAM_out,c_zx,c_nx,c_zy,c_ny,c_f,c_no,c_zr,c_ng,s_ALUout);
   
   P: pc port map (clock,'1',c_loadPC,reset,s_regAout,s_pcout);
 
